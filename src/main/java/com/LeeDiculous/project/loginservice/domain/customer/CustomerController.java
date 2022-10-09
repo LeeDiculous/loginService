0aaad60dec9f721edd620100c817354cf6dc3607
woo <evisahah@gmail.com>
2022-10-10 02:00:53 +0900

    10/10 2:00

--- src/main/java/com/LeeDiculous/project/loginservice/domain/customer/CustomerController.java
+++ src/main/java/com/LeeDiculous/project/loginservice/domain/customer/CustomerController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,4 +17,15 @@ public class CustomerController {
     @Autowired
     private final CustomerService customerService;
 
+    @GetMapping("/login")
+    public String loginForm() {
+
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String loginForm(@ModelAttribute Customer customer, Model model) {
+        model.addAttribute(customer);
+        return "login/loginHome";
+    }
 }
